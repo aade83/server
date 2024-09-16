25f0b99da0e93e5699afd96fa5b9ffe1022cd4b9
aade83 <162307406+aade83@users.noreply.github.com>
2024-09-16 20:51:14 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect(process.env.MONG0_URI);
-        console.log('mongodb connected successfully');
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+      console.log('mongodb connected successfully');
     }catch(error){
         console.log(error);
 
